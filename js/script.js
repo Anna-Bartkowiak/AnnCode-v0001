7d0d439c44a4c2f9b78f565a81bf8d8003b6ca2c
Anna B <aniabartkowiak19@op.pl>
2018-08-18 17:54:23 +0200

    Styling slider dots & js for active element in nav

--- js/script.js
+++ js/script.js
@@ -12,15 +12,15 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("dot");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-grey", "");
+     dots[i].className = dots[i].className.replace(" active", "");
   }
   x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-grey";
+  dots[slideIndex-1].className += " active";
 }
